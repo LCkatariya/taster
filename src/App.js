077139e4a30e9752df6f804b7ceafad3ca8dd21b
LCkatariya <lalchandkatariya91@gmail.com>
2022-01-06 17:07:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import { Routes, Route, Switch, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './home';
 import About from './about';
 
